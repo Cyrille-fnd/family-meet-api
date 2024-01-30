e79a9ab4b137d4d948bab7cbf54005c4690b7321
FERAND Cyrille <c.ferand@smallable.com>
2024-01-30 17:00:16 +0100

    :sparkles: add amqp message on user registration

---
+++ src/Message/RegisteredUserEvent.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Message;
+
+class RegisteredUserEvent
+{
+    private string $userId;
+
+    public function __construct(string $userId)
+    {
+        $this->userId = $userId;
+    }
+
+    public function getUserId(): string
+    {
+        return $this->userId;
+    }
+}
---
+++ src/MessageHandler/SendRegistrationMessageHandler.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\MessageHandler;
+
+use App\Entity\User;
+use App\Message\RegisteredUserEvent;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Mime\Email;
+
+#[AsMessageHandler]
+class SendRegistrationMessageHandler
+{
+    private MailerInterface $mailer;
+
+    private EntityManagerInterface $em;
+
+    public function __construct(
+        MailerInterface $mailer,
+        EntityManagerInterface $em
+    ) {
+        $this->mailer = $mailer;
+        $this->em = $em;
+    }
+
+    public function __invoke(
+        RegisteredUserEvent $event
+    ): void {
+        /** @var User|null */
+        $user = $this->em->getRepository(User::class)->find($event->getUserId());
+
+        if (null === $user) {
+            return;
+        }
+
+        $email = (new Email())
+        ->from('no-reply@family-meet.com')
+        ->to($user->getEmail())
+        ->subject(sprintf('Bienvenue %s %s !!', $user->getFirstname(), $user->getLastname()))
+        ->html('<p>Bonjour '.$user->getFirstname().',</p><p>Votre compte a été créé avec succès !!</p>');
+
+        $this->mailer->send($email);
+    }
+}
